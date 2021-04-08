abb8d0467fd045f8ab2b440df399ac2d494aa989
Shaina Muli <smuli@my.centennialcollege.ca>
2021-04-07 21:29:49 -0400

    Added Analytics.

---
+++ SlimeMazePro/Assets/Scripts/AnalyticsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsController : MonoBehaviour
+{
+    public string eventName;
+    public string key;
+    public string value;
+
+    //AnalyticsManager am;
+
+    public void recordEvent()
+    {
+        AnalyticsManager.instance.recordEvent(eventName);
+    }
+
+    public void recordEventDictionary()
+    {
+        AnalyticsManager.instance.recordEvent(eventName, key, value);
+    }
+
+}
---
+++ SlimeMazePro/Assets/Scripts/AnalyticsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    public static AnalyticsManager instance;
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void recordEvent(string eventname)
+    {
+        // event name = level complete
+        Analytics.CustomEvent(eventname);
+    }
+
+    public void recordEvent(string eventname, string key, string value)
+    {
+        // eventname = spell, key = water, walue = 40%
+        // eventname = skinsPurchased, key = character, value = skin2
+        Dictionary<string, object> eventParams = new Dictionary<string, object>();
+        eventParams.Add(key, value);
+        Analytics.CustomEvent(eventname, eventParams);
+
+    }
+}
