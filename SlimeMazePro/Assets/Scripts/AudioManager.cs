7ae3cbb828313354302dae088f84769e8019b359
SarcasticallyIronic <lucas.nicolaides21@gmail.com>
2021-04-08 03:18:56 -0400

    Added audio into game.

---
+++ SlimeMazePro/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public new AudioSource audio;
+
+
+    public void Play(AudioClip sfxToPlay)
+    {
+        if (audio.isPlaying)
+        {
+            audio.Stop();
+        }
+
+        audio.clip = sfxToPlay;
+        audio.Play();
+    }
+
+
+}
